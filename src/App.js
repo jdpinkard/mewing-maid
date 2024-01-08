32f7fe5d297faa6a6d052b928fd4670ccc66893e
Justin Pinkard <justin.d.pinkard@gmail.com>
2024-01-07 21:45:41 -0600

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from "react";
 import TTTgame from "./pages/TTTgame/TTTgame";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Mewing1s from "./assets/Mewing1s.jpg";
+import Mewing2s from "./assets/Mewing2s.jpg";
+import Mewing3s from "./assets/Mewing3s.jpg";
+
 
 import "./App.css";
 
@@ -14,11 +18,17 @@ const App = () => {
   }, [modalIsOpen]);
 
   return (
-        <div className={`main-container ${modalIsOpen && "modal-open"}`}>
-          <Header />
-          <TTTgame setModalIsOpen={setModalIsOpen} />
-          <Footer />
-        </div>
+    <div className="background-container">
+      <div className="background-noise"></div>
+      <img className="background-slides" src={Mewing1s} alt="Mewing Slide 1" />
+      <img className="background-slides" src={Mewing2s} alt="Mewing Slide 2" />
+      <img className="background-slides" src={Mewing3s} alt="Mewing Slide 3" />
+      <div className={`main-container ${modalIsOpen && "modal-open"}`}>
+        <Header />
+        <TTTgame setModalIsOpen={setModalIsOpen} />
+        <Footer />
+      </div>
+    </div>
   );
 };
 
